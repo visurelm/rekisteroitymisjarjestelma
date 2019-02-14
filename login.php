f395210c059d81c2a2a9da64aa674d91505b5715
anlinde <45077764+anlinde@users.noreply.github.com>
2019-02-14 19:28:52 +0200

    Anun uusimmat, cookiet ei toimi

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 include "header.html";
+require "header.html";
     $kayttajatunnus=$_POST["tunnus"];
 	$salasana=$_POST["salasana"];
 
@@ -7,13 +8,17 @@ include "header.html";
 $conn = new mysqli("127.0.0.1","trtkm18b3","trtkm18b3trtkm18b3","trtkm18b3");
 $sql = "SELECT kayttajatunnus, etunimi, sukunimi, kayttoikeustaso FROM jamke1 WHERE kayttajatunnus = '$kayttajatunnus' AND  salasana ='$salasana'";
 $result = $conn->query($sql);
-
 $res = mysqli_fetch_array($result);
+$etunimi = $row["etunimi"];
+
+
 
 if ($result->num_rows > 0) {
 	if ($res['kayttoikeustaso']=='B1'){
+	asetaKeksit();
 	header("Location: userView.php");
     exit;
+
 } 	else if ($res['kayttoikeustaso']=='A1'){
 	header("Location: adminView.php");
     exit;
